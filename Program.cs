efe5996db045d0b8d4299ab5f86d815afb9a2d76
taerhen <taerhen@outlook.com>
2022-03-23 22:39:01 +0300

    input handler + help

--- Program.cs
+++ Program.cs
@@ -1,155 +1,165 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace transport_idle
 {
     class Program
     {
-        
-        static Vehicle[] transPark = new [] {
-            new Vehicle {name = "taxi", price = 90, passangersMax = 1, incomePerPassanger = 5, incomeTimeSeconds = 3, availible = true},
-            new Vehicle {name = "bus", price = 20, passangersMax = 20, incomePerPassanger = 2, incomeTimeSeconds = 5},
-            new Vehicle {name = "trolley", price = 30, passangersMax = 30, incomePerPassanger = 2, incomeTimeSeconds = 7}
-            };
-        
-        static Dictionary<string, Func<>> commands = new Dictionary<string, Func<>> () 
-        {
-            {"buy", CheckBuy()},
-            {"exit", QuitGame()}
-        };
 
-        // static void ChooseAction (string actionInput)
-        // {
-        //     if (actionInput.Substring(0, 4) == "buy ")
-        //         foreach (var veh in transPark)
-        //         {
-        //             if (veh.name.Contains(actionInput.Substring(5)) && veh.availible)
-        //                 veh.Buy();
-        //                 break;
-        //         }
-        //         else if (actionInput == "exit")
-            
-        // }
-
-        static void CheckBuy(string item, Vehicle[] trans)
-        {
-            for (int i = 0; i < trans.Length; i++)
-            {
-                if (item.EndsWith(trans[i].name) && trans[i].availible)
-                    trans[i].Buy();
-                if (i + 1 > trans.Length && trans[i].amount >= trans[i].unlockNextAmount)
-                    trans[i + 1].availible = true;
-            }
-        }
-
-        static void QuitGame()
-        {
-            while (true)
-            {
-
-                Console.WriteLine("Do you wnt to leave? \ny / n");
-                string userInput = Console.ReadLine()??"";
-                if (userInput == "y")
-                    Environment.Exit(0);
-                else if (userInput == "n")
-                    return;
-                else
-                    Console.Write("Invalid input. ");
-            }
-        }
-        
         static void Main()
         {
             var timeUpdated = DateTime.Now;
             string actionInput;
+            Console.Clear();
 
-            Console.WriteLine(transPark[0].name.Contains("taxi"));
-                        
             while (true)
             {
-                World.Update(transPark);
-                World.PrintStatus(transPark);
+                
+                World.Update();
+                World.PrintStatus();
                 Console.WriteLine("What to do?");
                 actionInput = Console.ReadLine()??"";
+                var actionInputWords = actionInput.Split();
 
-                if (actionInput == "buy taxi" && World.CheckMoney(transPark[0]))
-                    {
-                        
-                        transPark[0].Buy();
-                        World.PrintStatus(transPark);
-                    }
-                else if(actionInput == "exit")
-                    break;
-                else
-                    Console.WriteLine("Unknown command, to buy taxi type 'buy taxi', to exit type 'exit'");
-            }
-
-            Console.WriteLine("Press ENTER to exit...");
-            actionInput = Console.ReadLine()??"";
-        }
-    }
-
-    static class World
-    {
-        
-        static public ulong playerMoney = 100;
-        
-        public static void Update(Vehicle[] transPark)
-        {
-            ulong transIncome = 0;
-
-            foreach (var veh in transPark)
-            {
-                if (veh.amount > 0)
+                switch (actionInput)
                 {
-                    ulong timePass = (ulong)DateTime.Now.Subtract(new DateTime()).TotalSeconds - veh.timeMoneyGained;
-                    ulong rounds = timePass / veh.incomeTimeSeconds;
-                    transIncome += rounds *
-                                    veh.amount *
-                                    veh.incomePerPassanger *
-                                    veh.passangersMax;
-                    veh.timeMoneyGained += rounds * veh.incomeTimeSeconds;
+                    case string s when s.StartsWith("buy "):
+                        Console.Clear();
+                        World.Buy(actionInputWords);
+                        break;
+                    
+                    case string s when s.StartsWith("help"):
+                        Console.Clear();
+                        foreach(KeyValuePair<string, string> kvp in World.commandList)
+                            Console.WriteLine(kvp.Key + kvp.Value);
+                        break;
+                    
+                    case string s when s.StartsWith("exit"):
+                        while (true)
+                        {
+                            Console.WriteLine("Do you want to leave? \ny / n");
+                            string userInput = Console.ReadLine()??"";
+                            if (userInput == "y")
+                                Environment.Exit(0);
+                            else if (userInput == "n")
+                            {
+                                Console.Clear();
+                                break;
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.Write("Invalid input. ");
+                            }
+                        }
+                        break;
+                    
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Unknown command, type 'help', to see availible commands");
+                        break;
+
                 }
             }
-                
-
-            playerMoney += transIncome;
-        } 
-
-        public static void PrintStatus(Vehicle[] transPark)
-        {
-            Console.WriteLine("Your money: {0}\nYou have {1} taxi.", playerMoney, transPark[0].amount);
         }
+    
 
-        public static bool CheckMoney(Vehicle veh)
+        static class World
         {
-            if (playerMoney >= veh.price)
-                return true;
-            return false;
-        }
-    }
-
-    public class Vehicle
-    {
-        public string name = "";
-        public uint amount = 0;
-        public uint price;
-        public uint passangersMax;
-        public uint incomePerPassanger;
-        public uint incomeTimeSeconds;
-        public ulong timeMoneyGained; 
-        public bool availible = false;
-        public uint unlockNextAmount = 10;
-        
-
-        public void Buy ()
-        {
-            if (amount == 0)
-                this.timeMoneyGained = (ulong)DateTime.Now.Subtract(new DateTime()).TotalSeconds;
             
-            World.playerMoney -= this.price;
-            this.amount ++;
-            price = (uint)(1.1 * price);
+            static public ulong playerMoney = 100;
+
+            static Vehicle[] transPark = new [] {
+                new Vehicle {name = "taxi", price = 50, passangersMax = 1, incomePerPassanger = 5, incomeTimeSeconds = 3, availible = true},
+                new Vehicle {name = "bus", price = 100, passangersMax = 20, incomePerPassanger = 2, incomeTimeSeconds = 5},
+                new Vehicle {name = "trolley", price = 150, passangersMax = 30, incomePerPassanger = 2, incomeTimeSeconds = 7}
+                };
+
+            static string[] vehTypes = new [] {"taxi", "bus", "trolley"};
+
+            public static Dictionary<string, string> commandList = new Dictionary<string, string>() 
+            {
+                {"buy <name>", "\t - \t buy specified vehicle"},
+                {"help\t", "\t - \t show list of commands"},
+                {"exit\t", "\t - \t close the game window"}
+            };
+            
+            public static void Update()
+            {
+                ulong transIncome = 0;
+
+                foreach (var veh in transPark)
+                {
+                    if (veh.amount > 0)
+                    {
+                        ulong timePass = (ulong)DateTime.Now.Subtract(new DateTime()).TotalSeconds - veh.timeMoneyGained;
+                        ulong rounds = timePass / veh.incomeTimeSeconds;
+                        transIncome += rounds *
+                                        veh.amount *
+                                        veh.incomePerPassanger *
+                                        veh.passangersMax;
+                        veh.timeMoneyGained += rounds * veh.incomeTimeSeconds;
+                    }
+                }
+
+                playerMoney += transIncome;
+            } 
+
+            public static void PrintStatus()
+            {
+                Console.WriteLine("Your money: {0}.", playerMoney);
+                foreach (var veh in transPark)
+                {
+                    if (veh.availible)
+                        Console.WriteLine("You have {0} {1}. To buy 1 more you need {2} money", veh.amount, veh.name, veh.price);
+                }
+            }
+
+            public static bool CheckMoney(Vehicle veh)
+            {
+                if (playerMoney >= veh.price)
+                    return true;
+                Console.WriteLine("Not enough money to buy {0}.", veh.name);
+                return false;
+            }
+
+            public static void Buy (string[] inputStringWords)
+            {
+                int index = Array.IndexOf(vehTypes, inputStringWords[inputStringWords.Length-1]);
+                Update();
+                if (index >= 0 && transPark[index].availible)
+                    {
+                        if (CheckMoney(transPark[index]))
+                            transPark[index].Buy();
+                        if (index + 1 < transPark.Length && transPark[index].amount >= transPark[index].unlockNextAmount)
+                            transPark[index + 1].availible = true;
+                    }
+                else Console.WriteLine("You can't buy '{0}'.", inputStringWords[inputStringWords.Length-1]);
+            }
+        }
+
+        public class Vehicle
+        {
+            public string name = "";
+            public uint amount = 0;
+            public uint price;
+            public uint passangersMax;
+            public uint incomePerPassanger;
+            public uint incomeTimeSeconds;
+            public ulong timeMoneyGained; 
+            public bool availible = false;
+            public uint unlockNextAmount = 10;
+            
+
+            public void Buy ()
+            {
+                if (amount == 0)
+                    this.timeMoneyGained = (ulong)DateTime.Now.Subtract(new DateTime()).TotalSeconds;
+                
+                World.playerMoney -= this.price;
+                this.amount ++;
+                price = (uint)(1.1 * price);
+            }
         }
     }
-
 }
