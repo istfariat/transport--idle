79aaf663c036dc7313398f9367bdff3d4431d88b
taerhen <taerhen@outlook.com>
2022-03-30 23:36:12 +0300

    sell mechanic & random passangers

--- Program.cs
+++ Program.cs
@@ -23,11 +23,16 @@ namespace transport_idle
 
                 switch (actionInput)
                 {
-                    case string s when s.StartsWith("buy "):
+                    case string s when s.StartsWith("buy"):
                         Console.Clear();
                         World.Buy(actionInputWords);
                         break;
                     
+                    case string s when s.StartsWith("sell"):
+                        Console.Clear();
+                        World.Sell(actionInputWords);
+                        break;
+
                     case string s when s.StartsWith("help"):
                         Console.Clear();
                         foreach(KeyValuePair<string, string> kvp in World.commandList)
@@ -35,6 +40,7 @@ namespace transport_idle
                         break;
                     
                     case string s when s.StartsWith("exit"):
+                        Console.Clear();
                         while (true)
                         {
                             Console.WriteLine("Do you want to leave? \ny / n");
@@ -67,15 +73,17 @@ namespace transport_idle
         static class World
         {
             
-            static public ulong playerMoney = 100;
+            static public ulong playerMoney = 10000;
+            static public double priceModifier = 1.1;
+            static public double sellModifier = 0.5;
 
             static Vehicle[] transPark = new [] {
-                new Vehicle {name = "taxi", price = 50, passangersMax = 1, incomePerPassanger = 5, incomeTimeSeconds = 3, availible = true},
-                new Vehicle {name = "bus", price = 100, passangersMax = 20, incomePerPassanger = 2, incomeTimeSeconds = 5},
-                new Vehicle {name = "trolley", price = 150, passangersMax = 30, incomePerPassanger = 2, incomeTimeSeconds = 7}
+                new Vehicle {name = "taxi", price = 50, passangersMax = 1, incomePerPassanger = 5, incomeTimeSeconds = 3, availible = true, fuelCost = 1.5},
+                new Vehicle {name = "bus", price = 100, passangersMax = 20, incomePerPassanger = 2, incomeTimeSeconds = 5, fuelCost = 5},
+                new Vehicle {name = "trolley", price = 150, passangersMax = 30, incomePerPassanger = 2, incomeTimeSeconds = 7, fuelCost = 3}
                 };
 
-            static string[] vehTypes = new [] {"taxi", "bus", "trolley"};
+            static string[] vehTypes = new string [] {"taxi", "bus", "trolley"};
 
             public static Dictionary<string, string> commandList = new Dictionary<string, string>() 
             {
@@ -94,10 +102,14 @@ namespace transport_idle
                     {
                         ulong timePass = (ulong)DateTime.Now.Subtract(new DateTime()).TotalSeconds - veh.timeMoneyGained;
                         ulong rounds = timePass / veh.incomeTimeSeconds;
-                        transIncome += rounds *
+                        Random rnd = new Random();
+                        double passangersAmount = rnd.Next(1, (int)veh.passangersMax);
+                        transIncome += (ulong)(rounds *
                                         veh.amount *
                                         veh.incomePerPassanger *
-                                        veh.passangersMax;
+                                        passangersAmount -
+                                        veh.fuelCost * 
+                                        rounds);
                         veh.timeMoneyGained += rounds * veh.incomeTimeSeconds;
                     }
                 }
@@ -136,6 +148,24 @@ namespace transport_idle
                     }
                 else Console.WriteLine("You can't buy '{0}'.", inputStringWords[inputStringWords.Length-1]);
             }
+
+            public static void Sell (string[] inputStringWords)
+            {
+                int index = Array.IndexOf(vehTypes, inputStringWords[inputStringWords.Length-1]);
+                Update();
+                if (index >= 0 && transPark[index].availible)
+                    {
+                        transPark[index].Sell();
+                        if (index + 1 < transPark.Length && transPark[index].amount <= transPark[index].unlockNextAmount)
+                            transPark[index + 1].availible = false;
+                    }
+                else Console.WriteLine("You can't sell '{0}'.", inputStringWords[inputStringWords.Length-1]);
+            }
+
+            public static void Buff ()
+            {
+                
+            }
         }
 
         public class Vehicle
@@ -149,6 +179,7 @@ namespace transport_idle
             public ulong timeMoneyGained; 
             public bool availible = false;
             public uint unlockNextAmount = 10;
+            public double fuelCost;
             
 
             public void Buy ()
@@ -158,8 +189,28 @@ namespace transport_idle
                 
                 World.playerMoney -= this.price;
                 this.amount ++;
-                price = (uint)(1.1 * price);
+                price = (uint)(World.priceModifier * price);
             }
+
+            public void Sell ()
+            {
+                if (amount == 0)
+                {
+                    Console.WriteLine("You have 0 units of this vehicle");
+                    return;
+                }
+
+                amount --;
+                World.playerMoney = (uint)(World.playerMoney + price * World.sellModifier);
+                price = (uint)(price / World.priceModifier);
+            }
+        }
+
+        public class Buff
+        {
+            public uint durationSeconds;
+            public DateTime startTime = DateTime.Now;
+
         }
     }
 }
